b71ca9e955d8aed844e05ee310edf25145144ee1
profjoaomenk <59892681+profjoaomenk@users.noreply.github.com>
2025-10-19 02:44:17 -0300

    Update UsuarioDto.cs

--- pricewhisper-usuarios.API/Models/DTOs/UsuarioDto.cs
+++ pricewhisper-usuarios.API/Models/DTOs/UsuarioDto.cs
@@ -3,10 +3,10 @@ namespace pricewhisper.Models.DTOs
     public class UsuarioDto
     {
         public int UsuarioId { get; set; }
-        public string Nome { get; set; }
-        public string NomeUsuario { get; set; }
-        public string Senha { get; set; }
+        public required string Nome { get; set; }
+        public required string NomeUsuario { get; set; }
+        public required string Senha { get; set; }
         public int EmpresaId { get; set; }
-        public string RazaoSocialEmpresa { get; set; }
+        public required string RazaoSocialEmpresa { get; set; }
     }
 }
