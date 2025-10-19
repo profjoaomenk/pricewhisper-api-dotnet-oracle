ef5e3b412e956502e75c93da7b6a72898832a35f
profjoaomenk <59892681+profjoaomenk@users.noreply.github.com>
2025-10-19 02:44:02 -0300

    Update EmpresaDto.cs

--- pricewhisper-usuarios.API/Models/DTOs/EmpresaDto.cs
+++ pricewhisper-usuarios.API/Models/DTOs/EmpresaDto.cs
@@ -3,9 +3,9 @@ namespace pricewhisper.Models.DTOs
     public class EmpresaDto
     {
         public int EmpresaId { get; set; }
-        public string CNPJ { get; set; }
-        public string RazaoSocial { get; set; }
-        public string NomeFantasia { get; set; }
-        public List<UsuarioDto> Usuarios { get; set; }
+        public required string CNPJ { get; set; }
+        public required string RazaoSocial { get; set; }
+        public required string NomeFantasia { get; set; }
+        public required List<UsuarioDto> Usuarios { get; set; }
     }
 }
